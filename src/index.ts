835469fb52a73a26280a5142e3a0fdfe388abc81
hyperz111 <firzeroun11@gmail.com>
2025-12-05 18:12:39 +0700

    perf: inline the replace function

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
-function replace(property: string, index: number): string {
-	let res = "";
-	let cursor = 0;
-
-	do {
-		res += property.substring(cursor, index) + (property[index + 1] as string).toUpperCase();
-		cursor = index + 2;
-		index = property.indexOf("-", cursor);
-	} while (~index);
-
-	return res + property.substring(cursor);
-}
-
 export default function camelCaseCss(property: string): string {
 	property = property.toLowerCase();
 
@@ -23,5 +10,15 @@ export default function camelCaseCss(property: string): string {
 		property = property.substring(1);
 		index = property.indexOf("-");
 	}
-	return replace(property, index);
+
+	let result = "";
+	let cursor = 0;
+
+	do {
+		result += property.substring(cursor, index) + (property[index + 1] as string).toUpperCase();
+		cursor = index + 2;
+		index = property.indexOf("-", cursor);
+	} while (~index);
+
+	return result + property.substring(cursor);
 }
